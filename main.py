f2b518fbb59442fdef31ec2c3905cc343acaa557
Glumov Andrey <gluman@yandex.ru>
2023-02-11 01:01:01 +0300

    11.02.2023 v1

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+import re
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
+# lastname,firstname,surname,organization,position,phone,email
 import csv
 with open("phonebook_raw.csv") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list)
+# pprint(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
+pattern = r"([А-ЯЁа-яё]+) ?([А-ЯЁа-яё]+)? ?([А-ЯЁа-яё]+)?,([А-ЯЁа-яё]+)? ?([А-ЯЁа-яё]+)?,([А-ЯЁа-яё]+)?,([А-ЯЁа-яё]*)?,([А-ЯЁа-яё \–\-\w]*)?,((\+?[\d ?]) ?(\(?\d{1,3}\)?)\-? ?(\d{1,3})\-?(\d{1,2})\-?(\d{1,2})([\(\)А-ЯЁа-яё \.\d]*)?)?,([\w\d\.]+\@[\w]+\.[\w]+)?,?'?\r?"
 # ваш код
+n_contacts = []
+n_cont = {}
+for contact_data in contacts_list[1:]:
+  for contact in contact_data[0].split(','):
+    n_cont['lastname']=
+
+    print(contact)
+
+#
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
